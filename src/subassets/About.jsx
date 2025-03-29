7f21d15c356767706c29b5d35434e725656f7ae9
Jojo <jvn.tan@gmail.com>
2025-03-29 18:55:39 +0700

    Changed amount of image viewed to start transition

--- src/subassets/About.jsx
+++ src/subassets/About.jsx
@@ -5,17 +5,36 @@ import { motion, useInView } from "framer-motion";
 const About = () => {
   return (
     <>
-      <div className="flex flex-col items-center h-[300vh]">
+      <div className="flex flex-col items-center h-[600vh]">
         <span className="sticky top-0 flex justify-center w-full bg-[#1E1E24] ">
           <h2 className=" text-[#FFF] font-bold p-10 pr-12 text-5xl">
             This is Me
           </h2>
         </span>
-        <motion.img
-          src={"/openEyes.png"}
-          alt="openEyesChingChong"
-          className=""
-        />
+        <div className="relative w-screen">
+          <motion.img
+            src={"/openEyes.png"}
+            alt="openEyesChingChong"
+            className="absolute z-[-10]"
+            initial={{
+              opacity: 1,
+            }}
+            transition={{
+              duration: 0.7,
+            }}
+            whileInView={{
+              opacity: 0,
+            }}
+            viewport={{
+              amount: 0.7,
+            }}
+          />
+          <motion.img
+            src={"/closedEyes.png"}
+            alt="openEyesChingChong"
+            className="absolute z-[-20]"
+          />
+        </div>
       </div>
     </>
   );
