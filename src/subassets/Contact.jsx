c37efd4e2bd1cd9f22405d121e1b85087a8597b4
Jojo <jvn.tan@gmail.com>
2025-04-02 02:23:49 +0700

    test

--- src/subassets/Contact.jsx
+++ src/subassets/Contact.jsx
@@ -28,7 +28,7 @@ const Contact = () => {
             <img
               className="w-8 h-8 rounded-full"
               src="./Jo.jpeg"
-              alt="Jo's image"
+              alt="Jo's images"
             />
             <div
               ref={ref}
